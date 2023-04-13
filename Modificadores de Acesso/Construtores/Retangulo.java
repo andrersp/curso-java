04b8e823e532a893187e49b0c8938af7237b640a
rspandre <rsp.assistencia@gmail.com>
2023-04-13 08:29:00 -0300

    Construtores

---
+++ Modificadores de Acesso/Construtores/Retangulo.java
@@ -0,0 +1,20 @@
+public class Retangulo {
+    private double largura;
+    private double altura;
+
+    public Retangulo() {
+        this(4, 3);
+        // largura = 4;
+        // altura = 3;
+    }
+
+    public Retangulo(double largura, double altura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public double calcularArea() {
+        return this.largura * this.altura;
+    }
+
+}
