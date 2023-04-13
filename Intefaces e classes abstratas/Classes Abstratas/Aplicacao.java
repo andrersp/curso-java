8ed50d7965dc7cf07a643f9721c0400c7e661183
rspandre <rsp.assistencia@gmail.com>
2023-04-13 16:58:40 -0300

    Classe abstrata

---
+++ Intefaces e classes abstratas/Classes Abstratas/Aplicacao.java
@@ -0,0 +1,17 @@
+public class Aplicacao {
+    public static void main(String[] args) {
+
+        Diamante d = new Diamante();
+        d.pegar();
+
+        Cogumelo c = new Cogumelo();
+        c.pegar();
+
+        Moeda m = new Moeda();
+        m.pegar();
+
+        Item ma = new Maca();
+        ma.pegar();
+
+    }
+}
---
+++ Intefaces e classes abstratas/Classes Abstratas/Item.java
@@ -0,0 +1,22 @@
+public abstract class Item {
+    private int posX;
+    private int posY;
+
+    public abstract void pegar();
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+}
