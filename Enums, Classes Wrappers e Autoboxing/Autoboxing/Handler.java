48db13254894dab4921c4bf93b45ab588692c64d
rspandre <rsp.assistencia@gmail.com>
2023-04-17 15:16:18 -0300

    Enums, Classes Wrappers e Autoboxing

---
+++ Enums, Classes Wrappers e Autoboxing/Autoboxing/Handler.java
@@ -0,0 +1,11 @@
+public class Handler {
+    private Object obj;
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+}
